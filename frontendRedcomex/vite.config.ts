5086594eb9a4718a6dff7e656ac975b8ed636de8
Jey Rodney <redes.ulloa@gmail.com>
2025-07-30 22:02:26 -0500

    Se agrega el back empaquetado

--- frontendRedcomex/vite.config.ts
+++ frontendRedcomex/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Esto permitirá que se acceda desde fuera de localhost
+    port: 5173,
+    allowedHosts: ['ec2-44-202-39-79.compute-1.amazonaws.com']
+  },
 })
